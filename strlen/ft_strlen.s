7a4baa1139a568ed0564b9831fe87bfac0860613
Cesar CLAUDE <cclaude@e1r10p22.42.fr>
2020-01-03 16:26:19 +0100

    started to grapple concepts

---
+++ strlen/ft_strlen.s
@@ -0,0 +1,21 @@
+; nasm -f macho64 ft_strlen.s && gcc -o exec main.c ft_strlen.o && ./exec
+
+section .data
+
+msg:    db      "Hello, world!", 10
+.len:   equ     $ - msg
+
+
+section .text
+	global _fstrlen
+
+_fstrlen:
+    mov     rax, 0x2000004 ; write
+    mov     rdi, 1 ; stdout
+    lea     rsi, [rel msg]
+    mov     rdx, msg.len
+    syscall
+
+    mov     rax, 0x2000001 ; exit
+    mov     rdi, 0
+    syscall
---
+++ strlen/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+extern void fstrlen(void);
+
+int	main(void)
+{
+	printf("In C program\n");
+	fstrlen();
+	return (0);
+}
