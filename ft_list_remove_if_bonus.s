8f3104877f820e03ea22c9a2bbf6017ed0322db2
YulonSil <richard.alpert.c@gmail.com>
2020-01-26 19:27:39 +0100

    chained lists in assembly make jack a dull boy

--- ft_list_remove_if_bonus.s
+++ ft_list_remove_if_bonus.s
@@ -1,35 +1,32 @@
 ; **************************************************************************** ;
 ;                                                                              ;
 ;                                                         :::      ::::::::    ;
-;    ft_list_sort_bonus.s                               :+:      :+:    :+:    ;
+;    ft_list_remove_if_bonus.s                          :+:      :+:    :+:    ;
 ;                                                     +:+ +:+         +:+      ;
 ;    By: cclaude <cclaude@student.42.fr>            +#+  +:+       +#+         ;
 ;                                                 +#+#+#+#+#+   +#+            ;
-;    Created: 2020/01/24 18:46:15 by cclaude           #+#    #+#              ;
-;    Updated: 2020/01/26 19:18:50 by cclaude          ###   ########.fr        ;
+;    Created: 2020/01/26 19:05:21 by cclaude           #+#    #+#              ;
+;    Updated: 2020/01/26 19:21:03 by cclaude          ###   ########.fr        ;
 ;                                                                              ;
 ; **************************************************************************** ;
 
-; nasm -f macho64 ft_list_sort_bonus.s && gcc -o exec main.c ft_list_sort_bonus.o && ./exec
+; nasm -f macho64 ft_list_remove_if_bonus.s && gcc -o exec main.c ft_list_remove_if_bonus.o && ./exec
 
 section .text
-	global _ft_list_sort
+	global _ft_list_remove_if
 
-_ft_list_sort:
-	mov		r12, rdi	; ptr to lst_start
+_ft_list_remove_if:
+	mov		r12, rdi	; list_start ptr
 	mov		rdi, [rdi]	; lst = *lst_start
-
 loop:
 	cmp		[rdi+8], 0	; if lst->next == NULL
 	je		end			; then return
 	push	rdi			;
-	push	rsi			;
 	mov		rcx, rsi	; store *cmp
 	mov		rsi, [rdi+8]; 2nd = lst->next
 	mov		rsi, [rsi]	; 2nd = lst->next->data
 	mov		rdi, [rdi]	; 1st = lst->data
-	call	rcx			; *cmp(lst->data, lst->next->data)
-	pop		rsi			;
+	call	rdx			; *cmp(lst->data, lst->next->data)
 	pop		rdi			;
 	test	rax, rax	; if rax > 0
 	jns		unsorted	; then unsorted
@@ -38,7 +35,7 @@ loop:
 unsorted:
 	mov		r10, [rdi+8]; lst->next
 	mov		r11, [r10+8]; lst->next->next
-	mov		[r12], r10	; before->next = next
+	mov		[r12], r10; before->next = next
 	mov		[r10+8], rsi; next->next = lst
 	mov		[rdi+8], r11; lst-> next = lst->next->next
 sorted:
