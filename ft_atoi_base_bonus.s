6d2b1b999e6925190e84d0df79cfcb223f704934
cclaude42 <cesarclaudepro@gmail.com>
2020-01-27 17:09:20 +0100

    starting work on atoi base

---
+++ ft_atoi_base_bonus.s
@@ -0,0 +1,19 @@
+; **************************************************************************** ;
+;                                                                              ;
+;                                                         :::      ::::::::    ;
+;    ft_atoi_base_bonus.s                               :+:      :+:    :+:    ;
+;                                                     +:+ +:+         +:+      ;
+;    By: cclaude <cclaude@student.42.fr>            +#+  +:+       +#+         ;
+;                                                 +#+#+#+#+#+   +#+            ;
+;    Created: 2020/01/27 17:08:04 by cclaude           #+#    #+#              ;
+;    Updated: 2020/01/27 17:09:05 by cclaude          ###   ########.fr        ;
+;                                                                              ;
+; **************************************************************************** ;
+
+; nasm -f macho64 ft_atoi_base_bonus.s && gcc -o exec main.c ft_atoi_base_bonus.o && ./exec
+
+section .text
+	global _ft_atoi_base
+
+_ft_atoi_base:
+	
